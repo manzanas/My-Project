854b8f02c233291727e68a1970c011eb7b42c8fd
manzanas <linnk_4@yahoo.com.mx>
2015-03-30 04:57:31 +0200

    rows

---
+++ rows.sql
@@ -0,0 +1,149 @@
+
+
+INSERT INTO USER_SYS(name_sys,nick,type_sys,pwd,date_up) VALUES('JUAN LOPEZ HERNANDEZ','JUANITO','ADMIN','123456','2014/10/22');
+INSERT INTO USER_SYS(name_sys,nick,type_sys,pwd,date_up) VALUES('JULIO HERNANDEZ PEREZ','JULIO','USER','123456','2014/10/23');
+INSERT INTO USER_SYS(name_sys,nick,type_sys,pwd,date_up) VALUES('PEDRO MEDINA','PEDRO','USER','123456','2014/10/29');
+INSERT INTO USER_SYS(name_sys,nick,type_sys,pwd,date_up) VALUES('KARLA HURTADO','KARLA','USER','123456','2014/11/5');
+
+
+INSERT INTO MEMBERSHIPS(type_memb,price_memb) VALUES('GOLDEN','1200');
+INSERT INTO MEMBERSHIPS(type_memb,price_memb) VALUES('SILVER','1000');
+INSERT INTO MEMBERSHIPS(type_memb,price_memb) VALUES('SILVER 2','800');
+INSERT INTO MEMBERSHIPS(type_memb,price_memb) VALUES('VISIT','0');
+
+
+
+
+
+
+
+
+
+INSERT INTO CUSTOMERS(NAME_C,celphone,DATE_BORN,SEX,id_memb) VALUES('AARÓN GARCIA CAMPOS','2201012086','2003-12-8','H','01');
+INSERT INTO CUSTOMERS(NAME_C,celphone,DATE_BORN,SEX,id_memb) VALUES('ABEL GONZALEZ VEGA','2244849938','2004-7-15','H','01');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ABELARDO RODRIGUEZ DIEZ','2292600634','1999-1-25','H','01');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ABIGAÍL FERNANDEZ FUENTES','2241065567','1988-4-26','M','01');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ABRAHAM LOPEZ CARRASCO','2286935275','1993-2-11','H','01');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ADOLFO MARTINEZ CABALLERO','2245335921','1984-3-15','H','01');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ADRIÁN SANCHEZ NIETO','2357388853','2001-9-11','H','01');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ADRIANA PEREZ REYES','2218951780','1973-1-27','M','01');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('AGUSTÍN GOMEZ AGUILAR','2266869097','2005-6-22','H','01');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ALAN MARTIN PASCUAL','2272877963','1971-8-20','H','01');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ALBA JIMENEZ HERRERO','2239601187','1996-5-26','M','01');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ALDO RUIZ SANTANA','2393237402','2003-6-2','H','02');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ALEJANDRA HERNANDEZ LORENZO','2296930107','1973-2-3','M','02');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ALEJANDRO DIAZ HIDALGO','2362527276','1994-9-11','H','02');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ALEXIS MORENO MONTERO','2392837782','2000-2-3','H','02');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ALFONSO ALVAREZ IBAÑEZ','2213255118','1983-10-18','H','02');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ALONDRA MUÑOZ GIMENEZ','2273688436','1991-6-13','M','02');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ALONSO ROMERO FERRER','2359135862','1997-7-16','H','v');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ALVARO ALONSO DURAN','2211376062','2005-12-20','H','02');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('AMPARO NAVARRO VICENTE','2207358774','1975-7-1','H','02');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ANA TORRES BENITEZ','2332203209','1994-4-13','M','02');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ANDREA DOMINGUEZ MORA','2289743409','1971-3-12','M','02');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ANGELA VAZQUEZ SANTIAGO','2246239158','1998-1-19','M','02');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ANGELES RAMOS ARIAS','2244847601','1988-4-13','M','02');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ANGÉLICA GIL VARGAS','2247091410','1999-12-4','M','02');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ANNEL RAMIREZ CARMONA','2300486427','1982-1-17','M','02');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ANTONIA SERRANO CRESPO','2266692013','1982-4-21','M','02');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ARACELI BLANCO ROMAN','2343504390','1993-9-7','M','02');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ARELY SUAREZ PASTOR','2240920665','1992-4-4','M','02');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ARIEL MOLINA SOTO','2203338547','1999-10-16','H','02');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ARMANDO MORALES SAEZ','2310515177','1996-2-7','H','03');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('ARTURO ORTEGA VELASCO','2380216502','2001-1-16','H','03');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('BENJAMÍN DELGADO SOLER','2251036218','2001-11-20','H','03');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('BERENICE CASTRO MOYA','2313249907','1998-1-12','M','03');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('BERNARDO ORTIZ ESTEBAN','2375529444','1989-3-17','H','03');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('BERTHA RUBIO PARRA','2379743655','2005-6-20','M','03');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('BLANCA MARIN BRAVO','2289420738','1979-3-27','M','03');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('BRANDON SANZ GALLARDO','2218579563','1982-2-27','H','03');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('BRENDA NUÑEZ ROJAS','2342199065','1979-7-10','M','03');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('BRIAN IGLESIAS ROMERO','2279271085','1995-11-27','H','03');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('CARLOS MEDINA ALONSO','2350170178','1982-1-19','H','03');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('CARMINA GARRIDO NAVARRO','2275967080','1975-11-1','M','03');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('CAROLINA SANTOS TORRES','2285383730','1980-12-11','M','03');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('CATALINA CASTILLO DOMINGUEZ','2230126320','1984-8-16','M','03');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('CÉSAR CORTES VAZQUEZ','2373038533','1971-2-1','H','03');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('CHRISTIAN LOZANO RAMOS','2309408412','1980-7-24','H','03');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('CHRISTOPHER GUERRERO GIL','2243406186','1970-1-1','H','03');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('CINDY CANO RAMIREZ','2298603243','1973-4-12','M','03');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('CLAUDIA PRIETO SERRANO','2276375401','1982-8-5','M','03');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('CLAUDIO MENDEZ BLANCO','2380041535','1999-2-1','M','03');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('CONSUELO CALVO SUAREZ','2327495748','2004-1-19','M','03');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('CRISTÓBAL CRUZ MOLINA','2383138420','1974-9-12','H','03');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('CRYSTELL GALLEGO MORALES','2333074196','1991-1-17','M','03');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('CYNTHIA VIDAL ORTEGA','2259706307','1991-10-8','M','03');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('DANIELA LEON CANO','2324415765','2004-2-14','M','03');
+INSERT INTO CUSTOMERS(NAME_C,CELL,DATE_BORN,SEX,HEIGHT) VALUES('DARÍO HERRERA PRIETO','2370904515','2000-1-5','H','03');
+
+
+
+INSERT INTO PRODUCTS(name_p,type_prod,trade_mark,pres,descrip,price,quantity) VALUES('Nitro Tech','Proteina','Muscletech','907 Grs','Polvo para preparar bebida con monohidrato de creatina Sabor chocolate. Agregue una cucharada (36 g) en 200 ml de agua fría mezcle y tome una vez al día después del ejercicio. No exceda de la porción recomendada al día. Tamaño de la porción: 1 cucharada (36 g). Porciones por envase: 25 Aprox','880','10');
+INSERT INTO PRODUCTS(name_p,type_prod,trade_mark,pres,descrip,price,quantity) VALUES('Syntha-6','Proteina','BSN','1320 Grs','Fórmula en polvo para preparar bebida elaborada de Proteína de Suero de Leche. Agregue una cucharada (47 g) en 150 ml de agua fría o jugo, mezcle y tome de una o dos veces al día. No exceda de la porción recomendada al día. Tamaño de la porción: 1 cucharada (47 g). Porciones por envase: 28 Aprox.','759','5');
+INSERT INTO PRODUCTS(name_p,type_prod,trade_mark,pres,descrip,price,quantity) VALUES('Syntha-6','Proteina','BSN','1320 Grs','Fórmula en polvo para preparar bebida elaborada de Proteína de Suero de Leche. Agregue una cucharada (47 g) en 150 ml de agua fría o jugo, mezcle y tome de una o dos veces al día. No exceda de la porción recomendada al día. Tamaño de la porción: 1 cucharada (47 g). Porciones por envase: 28 Aprox.','759','5');
+INSERT INTO PRODUCTS(name_p,type_prod,trade_mark,pres,descrip,price,quantity) VALUES('100 % Whey Protein','Proteina','EAS Sport Nutrition','2411 Grs','Fórmula elaborada con 26 gramos de proteína y aminoácidos. Contiene Aminoacidos de Cadena Ramificada-BCAA (L-Isoleucina, L-Leucina y L-Valina). Adicionada con minerales. Agregue dos cucharadas (39 g) en 240 ml de agua fría o su bebida preferida, mezcle de 20 a 30 segundos en una licuadora y tome durante el ejercicio una o dos veces al día. No exceda de la porción recomendada al día. Tamaño de la porción: 2 cuchardas. (39g). Porciones por envase: 58 Aprox.','1200','3');
+INSERT INTO PRODUCTS(name_p,type_prod,trade_mark,pres,descrip,price,quantity) VALUES('101 % Whey Protein','Proteina','EAS Sport Nutrition','2412 Grs','Fórmula elaborada con 26 gramos de proteína y aminoácidos. Contiene Aminoacidos de Cadena Ramificada-BCAA (L-Isoleucina, L-Leucina y L-Valina). Adicionada con minerales. Agregue dos cucharadas (39 g) en 240 ml de agua fría o su bebida preferida, mezcle de 20 a 30 segundos en una licuadora y tome durante el ejercicio una o dos veces al día. No exceda de la porción recomendada al día. Tamaño de la porción: 2 cuchardas. (39g). Porciones por envase: 58 Aprox.','1200','3');
+INSERT INTO PRODUCTS(name_p,type_prod,trade_mark,pres,descrip,price,quantity) VALUES('ISO ZERO','Proteina','Muscletech','907 Grs','Polvo para preparar bebida con 25 gramos de proteína y 4,6 gramos en BCAAs por porción. No contiene Azúcar ni grasas. 0 carbohidratos. Adiciona mezcla de enzimas. Sabor a chocolate. Agregue una cucharada (32 g) en 200 ml de agua fría, mezcle y tome una o dos veces al día entre comidas o antes y después del ejercicio. No exceda de la porción recomendada al día. Tamaño de la porción: 1 cucharada (32 g). Porciones por envase: 28 Aprox.','999','4');
+INSERT INTO PRODUCTS(name_p,type_prod,trade_mark,pres,descrip,price,quantity) VALUES('ISO ZERO','Proteina','Muscletech','907 Grs','Polvo para preparar bebida con 25 gramos de proteína y 4,6 gramos en BCAAs por porción. No contiene Azúcar ni grasas. 0 carbohidratos. Adiciona mezcla de enzimas. Sabor a chocolate. Agregue una cucharada (32 g) en 200 ml de agua fría, mezcle y tome una o dos veces al día entre comidas o antes y después del ejercicio. No exceda de la porción recomendada al día. Tamaño de la porción: 1 cucharada (32 g). Porciones por envase: 28 Aprox.','999','8');
+INSERT INTO PRODUCTS(name_p,type_prod,trade_mark,pres,descrip,price,quantity) VALUES('True - Mass','Proteina','BSN','5.75 lbs','Fórmula en polvo para preparar bebida con Proteína de Suero de Leche y Aminoacidos de Cadena Ramificada (BCAA). Agregue tres cucharadas (165 g) en 500 ml de agua fría o de su bebida favorita, mezcle y tome una vez al día. No exceda de la porción recomendada al día. Tamaño de la porción: 3 cucharadas (165 g). Porciones por envase: 16 Aprox.','1099','7');
+INSERT INTO PRODUCTS(name_p,type_prod,trade_mark,pres,descrip,price,quantity) VALUES('True - Mass','Proteina','BSN','5.75 lbs','Fórmula en polvo para preparar bebida con Proteína de Suero de Leche y Aminoacidos de Cadena Ramificada (BCAA). Agregue tres cucharadas (165 g) en 500 ml de agua fría o de su bebida favorita, mezcle y tome una vez al día. No exceda de la porción recomendada al día. Tamaño de la porción: 3 cucharadas (165 g). Porciones por envase: 16 Aprox.','1099','7');
+INSERT INTO PRODUCTS(name_p,type_prod,trade_mark,pres,descrip,price,quantity) VALUES('BSN Cellmass','Creatina','BSN','640 Grs','','759','5');
+INSERT INTO PRODUCTS(name_p,type_prod,trade_mark,pres,descrip,price,quantity) VALUES('CE C4','Creatina','Cellucor','174','','759','5');
+INSERT INTO PRODUCTS(name_p,type_prod,trade_mark,pres,descrip,price,quantity) VALUES('CE C5','Creatina','Cellucor','175','','1200','3');
+INSERT INTO PRODUCTS(name_p,type_prod,trade_mark,pres,descrip,price,quantity) VALUES('Cell Tech','Creatina','Muscletech','3 lbs','','1200','3');
+INSERT INTO PRODUCTS(name_p,type_prod,trade_mark,pres,descrip,price,quantity) VALUES('Cell Tech','Creatina','Muscletech','3 lbs','','999','4');
+INSERT INTO PRODUCTS(name_p,type_prod,trade_mark,pres,descrip,price,quantity) VALUES('Creatine Hardcore','Creatina','Muscletech','300 grs','','769','324');
+
+
+
+
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','9:13:13','9:13:13','1');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','7:18:19','7:18:19','21');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','6:24:50','6:24:50','6');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','18:20:37','18:20:37','5');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','7:11:43','7:11:43','34');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','15:20:0','15:20:0','2');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','16:19:27','16:19:27','31');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','17:16:43','17:16:43','17');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','17:16:34','17:16:34','32');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','18:14:55','18:14:55','50');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','18:20:31','18:20:31','25');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','9:6:31','9:6:31','19');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','12:22:25','12:22:25','10');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','13:11:9','13:11:9','45');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','8:20:37','8:20:37','1');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','15:11:32','15:11:32','3');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','13:8:19','13:8:19','48');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','9:8:11','9:8:11','10');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','13:24:32','13:24:32','40');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','8:6:29','8:6:29','15');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','15:16:33','15:16:33','34');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','14:6:57','14:6:57','48');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','10:10:17','10:10:17','52');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','12:16:59','12:16:59','8');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','19:23:14','19:23:14','39');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','7:22:57','7:22:57','52');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','19:18:26','19:18:26','54');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','7:21:32','7:21:32','37');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','17:20:46','17:20:46','26');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','14:16:1','14:16:1','44');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','16:13:13','16:13:13','10');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','8:15:14','8:15:14','27');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','10:21:13','10:21:13','28');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','17:11:46','17:11:46','40');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','17:16:55','17:16:55','55');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','20:17:49','20:17:49','47');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','15:7:53','15:7:53','47');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','14:22:17','14:22:17','46');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','11:20:58','11:20:58','1');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','18:11:27','18:11:27','51');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','15:6:1','15:6:1','53');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','6:10:52','6:10:52','26');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','16:22:40','16:22:40','28');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','14:11:12','14:11:12','36');
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','8:19:3','8:19:3','27');
+
+
+INSERT INTO PRESENCES(date_press,checkin,checkout,id_cust) VALUES('2014/10/21','12:21:18','18:21:12','15');
+
+
